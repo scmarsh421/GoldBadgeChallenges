3fdd70cb4034de592a2d3f7188ff32a68e75a6b4
Spencer Marsh <scmarsh421@gmail.com>
2021-11-14 16:07:45 -0500

    Initial Commit

---
+++ GoldBadgeChallenges/01-Cafe_Repository/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Cafe_Repository
+{
+    public class Menu
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string MealDescription { get; set; }
+        public string MealIngredients { get; set; } //could a list be a property?
+        public decimal MealPrice { get; set; }
+
+        public Menu() { } 
+        public Menu(int mealNumber, string mealName, string mealDescription, string mealIngredients, decimal mealPrice)
+        {
+            MealNumber = mealNumber;
+            MealName = mealName;
+            MealDescription = mealDescription;
+            MealIngredients = mealIngredients;
+            MealPrice = mealPrice;
+        }
+
+    }
+}
---
+++ GoldBadgeChallenges/01-Cafe_Repository/MenuRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Cafe_Repository
+{
+    public class MenuRepository
+    {
+        private List<Menu> _listOfMenuItems = new List<Menu>();
+
+        //Create
+        public void AddItemToMenuList(Menu menu)
+        {
+            _listOfMenuItems.Add(menu);
+        }
+
+        //Read
+        public List<Menu> GetMenuItemsList()
+        {
+            return _listOfMenuItems;
+        }
+        
+        //Delete
+        public bool RemoveMenuItemFromList(string mealName)
+        {
+            Menu menu = GetMenuItemByMealName(mealName);
+            
+            if(menu == null)
+            {
+                return false;
+            }
+            
+            
+
+        }
+
+        //Get Item by mealName
+        private Menu GetMenuItemByMealName(string mealName)
+        {
+            foreach (Menu menu in _listOfMenuItems)
+            {
+                if(menu.MealName ==mealName)
+                {
+                    return menu;
+                }
+            }
+            return null;
+        }
+    }
+}
