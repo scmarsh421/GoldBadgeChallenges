d9b21e623c7a702d75a798b61d3226789e37a106
Spencer Marsh <scmarsh421@gmail.com>
2021-11-15 23:14:29 -0500

    Redoing last push unsaved unit test

--- GoldBadgeChallenges/01-Cafe_Tests/CafeTests.cs
+++ GoldBadgeChallenges/01-Cafe_Tests/CafeTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using _01_Cafe_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace _01_Cafe_Tests
@@ -6,12 +7,12 @@ namespace _01_Cafe_Tests
     [TestClass]
     public class CafeTests
     {
+        MenuRepository menuRepoTest = new MenuRepository();
 
         [TestMethod]
         public void AddItem()
         {
-            MenuRepository menuRepoTest = new MenuRepository();
-            
+
         }
     }
 }
